6377aff4b95362bcb17da63300366b62c3fa228d
Priti Mondol <pritilata@hotmail.com>
2024-06-22 22:12:58 -0700

    Modified gitignore file

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,5 +1,5 @@
 import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
-import React from 'react'
+
 
 function ColorModeSwitch() {
     const {toggleColorMode, colorMode} = useColorMode()
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,35 +1,42 @@
-import { FaWindows, FaPlaystation,FaXbox, FaApple, FaLinux, FaAndroid} from'react-icons/fa'
-import { MdPhoneIphone} from 'react-icons/md'
-import { SiNintendo } from 'react-icons/si';
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
 
-import {BsGlobe } from 'react-icons/bs'
-import { HStack, Icon, Text} from '@chakra-ui/react';
-import { Platform } from '../hooks/useGames';
-import { IconType } from 'react-icons';
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
 
-type Props ={
-    platforms: Platform[],
-}
-function PlatformIconList( {platforms} : Props) {
-const iconMap: { [key: string] : IconType}= {
+type Props = {
+  platforms: Platform[];
+};
+function PlatformIconList({ platforms }: Props) {
+  const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
-    playStation: FaPlaystation,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
     linux: FaLinux,
+    ios: MdPhoneIphone,
     android: FaAndroid,
     web: BsGlobe,
-    xbox: FaXbox,
-    mac: FaApple,
-    nintendo: SiNintendo
-
-}
+  };
 
   return (
-    <HStack marginY='1'>
-      { platforms.map( platform=> (
-        <Icon key={platform.id} color='gray-500' as={ iconMap[platform.slug] } />)) 
-      }
+    <HStack marginY="1">
+      {platforms.map((platform) => (
+        <Icon key={platform.id} color="gray-500" as={iconMap[platform.slug]} />
+      ))}
     </HStack>
-  )
+  );
 }
 
-export default PlatformIconList
+export default PlatformIconList;
